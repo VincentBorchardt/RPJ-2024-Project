bae179b5ba2158248576f48572cd24aad0d9c0cd
VincentBorchardt <vborchardt123@gmail.com>
2024-08-06 13:34:28 -0500

    Initial work on cutscenes, including Messages and Persons
    I had the epiphany of how I'm supposed to handle cutscenes, and it involves passing Message resources to the cutscenes (using an MessageManager of some kind), and this is the initial work on that, mostly just baseline (though there's some look/feel on the cutscenes).

---
+++ scripts/resources/message.gd
@@ -0,0 +1,13 @@
+class_name Message extends Resource
+
+@export var speaker : Person
+@export var message : String
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
---
+++ scripts/resources/person.gd
@@ -0,0 +1,14 @@
+class_name Person extends Resource
+
+@export var person_name : String
+@export var full_picture : Texture2D
+@export var customer_picture : Texture2D
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	pass
