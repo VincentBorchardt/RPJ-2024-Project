fe44f2944c11541613b7272197adad0419983db1
VincentBorchardt <vborchardt123@gmail.com>
2024-08-15 23:35:02 -0500

    New SplashScreens, ProgressBar added
    Splash screens for Levels 2 and 3 added, all have a progress bar so there's more visible reaction to what you're doing. Level 2 cutscene is next, as is instructions and Level 2 proper.

--- scripts/cutscenes/splash_manager.gd
+++ scripts/cutscenes/splash_manager.gd
@@ -2,6 +2,9 @@ extends Control
 
 @export var next_scene : PackedScene
 
+@onready var splash_timer = $SplashTimer
+@onready var progress_bar = $ProgressBar
+
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
@@ -9,7 +12,8 @@ func _ready():
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	var percent = (1 - (splash_timer.time_left / splash_timer.wait_time)) * 100
+	progress_bar.value = percent
 
 
 func _on_splash_timer_timeout():
